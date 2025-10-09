38005970b6100957f96f4ca5b08e1877b8facb01
Liu Chang <167509197+luluyuu@users.noreply.github.com>
2025-10-09 21:10:23 +0800

    Update validation training script

--- train-Dronevehicle.py
+++ train-Dronevehicle.py
@@ -9,4 +9,4 @@ os.environ["TORCH_CPP_LOG_LEVEL"] = "ERROR"  # 屏蔽 INFO/WARNING 日志
 model = YOLO("./ultralytics/cfg/models/COMO/yolov8s_como.yaml",)  # load a pretrained model (recommended for training)
 model.train(data="./ultralytics/cfg/datasets/DroneVehicle.yaml", epochs=100, imgsz=640, name='./run/train/yolov8s-one-mamba', batch=128, device="0,1,2,3")  # trai
 # validate
-# model.val(data="ODinMJ.yaml", conf=0.25, iou=0.65, task='val', save_json=True, name='./run/val/exp')  # validate
+# model.val(data="./ultralytics/cfg/datasets/DroneVehicle.yaml", name='./run/val/exp')  # validate
